c5eb2cc0ecb5720aed4e717f7c92dfe7cfd4ad47
Ryan Neil <59746024+ryan-neil@users.noreply.github.com>
2021-02-10 15:43:46 -1000

    Add files via upload

---
+++ destructuring.js
@@ -0,0 +1,132 @@
+/*
+JavaScript Notes and Documentation
+Destructuring:
+*/
+
+/*
+==========================================
+Destructuring
+==========================================
+- A short, clean syntax to "unpack",
+  * Values from arrays
+  * Properties from objects
+into distinct variables.
+*/
+
+/*
+Destructuring Arrays
+
+- Order does matter for arrays
+*/
+const raceResults = [
+	'Ryan Neil',
+	'Katie Neil',
+	'Galen Rupp',
+	'Alphonce Simbu',
+	'Jared Ward'
+];
+// saving names to a variable pre-destructuring:
+const gold = raceResults[0]; // -> Ryan Neil
+const silver = raceResults[1]; // -> Katie Neil
+const bronze = raceResults[2]; // -> Galen Rupp
+
+// destructuring method:
+const [ gold, silver, bronze ] = raceResults;
+console.log(gold); // -> Ryan Neil
+console.log(silver); // -> Katie Neil
+console.log(bronze); // -> Galen Rupp
+
+const [ first, , , fourth ] = raceResults;
+console.log(fourth); // -> Alphonce Simbu
+
+// using "rest" inside our destructuring array:
+const [ winner, ...runnerUps ] = raceResults;
+console.log(winner); // -> Ryan Neil
+console.log(runnerUps); // -> [ "Katie Neil", "Galen Rupp", "Alphonce Simbu", "Jared Ward" ]
+
+/*
+Destructuring Objects
+
+- Order does not matter for objects
+- The variable names must be ket names from the object
+*/
+const runner = {
+	first   : 'Katie',
+	last    : 'Neil',
+	country : 'United States',
+	title   : 'Lord of All Runners'
+};
+const { first, last, country } = runner;
+console.log(first); // -> "Katie"
+console.log(last); // -> "Neil"
+console.log(country); // -> "United States"
+
+// giving the keys new names:
+const { country: nation } = runner;
+console.log(nation); // -> "United States"
+console.log(country); // -> undefined
+
+// using "rest" inside our destructuring object:
+const { first, last, ...others } = runner;
+console.log(first); // -> "Katie"
+console.log(last); // -> "Neil"
+console.log(others); // -> { country: "United States", title: "Lord of All Runners" }
+
+/*
+Nested Destructuring
+*/
+const results = [
+	{
+		first   : 'Katie',
+		last    : 'Neil',
+		country : 'United States'
+	},
+	{
+		first   : 'Ryan',
+		last    : 'Neil',
+		country : 'Canada'
+	},
+	{
+		first   : 'Galen',
+		last    : 'Rupp',
+		country : 'Belgium'
+	}
+];
+
+// Example 1: Extract the country of second place
+const [ , { country } ] = results;
+console.log(country); // -> "Canada"
+// Example 1.1: Extract the first name and country of third place
+const [ , , { first, country } ] = results;
+console.log(first, country); // -> Galen Belgium
+
+/*
+Destructuring Parameters
+
+- We often find destructuring inside a function definition where the parameters are listed.
+- If we use the same syntax inside the parentheses of a function definition we can extract or "unpack" values from the arguments passed in.
+*/
+
+// Example 1: Destructuring Parameters with Objects
+// here we're expecting "fullName" to be passed an object hence the curly braces
+const fullName = ({ first, last }) => {
+	return `${first} ${last}`;
+};
+
+const fullProfile = ({ first, last, country }) => {
+	return `${first} ${last}, ${country}`;
+};
+const runner = {
+	first   : 'Katie',
+	last    : 'Neil',
+	country : 'United States'
+};
+fullName(runner); // -> Katie Neil
+fullProfile(runner); // -> Katie Neil, United States
+
+// Example 2: Destructuring Parameters with Arrays (HTTP)
+const response = [ 'HTTP/1.1', '200 OK', 'application/json' ];
+function parseResponse([ protocol, statusCode, contentType ]) {
+	console.log(`Status: ${statusCode}`);
+}
+parseResponse(response); // -> Status: 200 OK
--- spread-rest.js
+++ spread-rest.js
@@ -1,4 +1,9 @@
 /*
+JavaScript Notes and Documentation
+Spread and Rest:
+*/
+
+/*
 ==========================================
 Spread
 ==========================================
@@ -23,33 +28,33 @@ Math.max(...nums); // -> 9 (this is the same as calling: Math.max(9, 3, 2, 8))
 
 // Example 1:
 function giveMeFour(a, b, c, d) {
-	console.log('a', a);
-	console.log('b', b);
-	console.log('c', c);
-	console.log('d', d);
+	console.log('a:', a);
+	console.log('b:', b);
+	console.log('c:', c);
+	console.log('d:', d);
 }
 const colors = [ 'red', 'orange', 'yellow', 'green' ];
 // without spread
 giveMeFour(colors);
-// -> a [ 'red', 'orange', 'yellow', 'green' ]
-// -> b undefined
-// -> c undefined
-// -> d undefined
+// -> a: [ 'red', 'orange', 'yellow', 'green' ]
+// -> b: undefined
+// -> c: undefined
+// -> d: undefined
 
 // with spread
 giveMeFour(...colors);
-// -> a red
-// -> b orange
-// -> c yellow
-// -> d green
+// -> a: red
+// -> b: orange
+// -> c: yellow
+// -> d: green
 
 // spread with a string
 const str = 'GOAT';
 giveMeFour(...str);
-// -> a G
-// -> b O
-// -> c A
-// -> d T
+// -> a: G
+// -> b: O
+// -> c: A
+// -> d: T
 
 /*
 Spread in Array Literals
@@ -130,9 +135,84 @@ const houseCatClone = {
 
 /*
 ==========================================
+The Arguments Object
+==========================================
+- Available inside every function.
+- It's an array-like object
+	* Has a length property
+	* Does not have array methods like push/pop
+- Contains all the arguments passed to the function
+- Not available inside of arrow functions
+*/
+
+// old method to sum all numbers
+function sumAll() {
+	let total = 0;
+	for (let i = 0; i < arguments.length; i++) {
+		total += arguments[i];
+	}
+	return total;
+}
+sumAll(8, 4, 6, 3, 4); // -> 25
+sumAll(2, 3); // -> 5
+
+// reduce method
+function sumAll() {
+	// for this method we must turn arguments into an array if we want to use array methods, like .reduce()
+	const argsArr = [ ...arguments ];
+	return argsArr.reduce((total, currVal) => {
+		return total + currVal;
+	});
+}
+sumAll(2, 2, 2); // -> 6
+
+/*
+==========================================
 Rest
 ==========================================
 - Collects arguments or data down into a single array.
 - Almost like the opposite of "spread".
 - Primarily used when we want to make functions that accept an unlimited number of arguments or a variable number of arguments.
 */
+function sumAll(...nums) {
+	let total = 0;
+	for (let n of nums) {
+		total += n;
+	}
+	return total;
+}
+sumAll(1, 2); // -> 3
+
+// .reduce() method
+function sumAll(...nums) {
+	// this time we do not need to make it an array because ...nums already does this for us
+	return nums.reduce((total, currVal) => {
+		return total + currVal;
+	});
+}
+sumAll(3, 4); // -> 7
+
+// Example 1:
+function fullName(first, last, ...titles) {
+	console.log('first:', first);
+	console.log('last:', last);
+	console.log('titles:', titles);
+}
+fullName('tom', 'jones', 'III', 'order of the phoenix');
+// -> first: tom
+// -> last: jones
+// -> titles: [ "III", "order of the phoenix" ]
+// ** after "tom" and "jones" the rest of the parameters get caught in "...titles" **
+
+// Example 2: Multiply all numbers of an array
+// implicit method
+const multiplyFunc = (...nums) =>
+	nums.reduce((total, currVal) => {
+		return total * currVal;
+	});
+multiplyFunc(10, 10); // -> 100
+// or
+// double implicit method (one line)
+const multiplyFunc = (...nums) =>
+	nums.reduce((total, currVal) => total * currVal);
+multiplyFunc(2, 10); // -> 20
