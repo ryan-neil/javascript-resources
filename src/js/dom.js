e6ca58a82d79b76fb5832c67f0adc3550916443f
Ryan Neil <ryanjosephneil@gmail.com>
2021-02-26 22:36:19 -1000

    added dom file.

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,10 +1,37 @@
 /*
 JavaScript Notes and Documentation
-The Dom:
+The Document Object Model
 */
 
 /*
 ====================================
 The Dom
 ====================================
+- This is, fundamentally, connecting JavaScript to HTML and CSS.
+- It's our JavaScript window into the contents of a webpage.
+- It's just a bunch of objects that we can interact with via JavaScript.
+- It is the JavaScript representation of the content on a page and it consists of a bunch of JavaScript objects.
 */
+
+/*
+====================================
+Document
+====================================
+ - The document object is our entry point into the world of the DOM. It contains representations of all the content on a page, plus tons of useful methods and properties.
+ - Document is the root of the entire webpage. It's in every browser window we open (<!document html>).
+ - All of the other pieces of the DOM are located within the "document" object.
+*/
+
+/*
+====================================
+DOM Methods
+====================================
+*/
+
+// *** Selecting:
+
+// getElementById
+
+// getElementsByTagName
+
+// getElementsByClassName
