36d29579fb6daee15d40bf0730e609f00467b33d
Ryan Neil <ryanjosephneil@gmail.com>
2021-02-28 23:24:11 -1000

    updated readme.

--- src/js/console-testing/test.js
+++ src/js/console-testing/test.js
@@ -1,2 +1,15 @@
 // Practice
 // Cmd + Option + K (Console Shortcut)
+
+// JavaScript
+const range = document.querySelector('input[type="range"]');
+
+// new max attribute
+range.setAttribute('max', '120');
+const newMax = range.max;
+console.log(newMax);
+
+// new min attribute
+range.setAttribute('min', '-10');
+const newMin = range.min;
+console.log(newMin);
