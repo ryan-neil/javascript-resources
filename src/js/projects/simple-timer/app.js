372f59d07ab75809dc122746b3e7e6de6b3f92dc
Ryan Neil <ryanjosephneil@gmail.com>
2021-05-27 23:17:50 -1000

    fix: folder and file structure

--- src/js/projects/simple-timer/app.js
+++ src/js/projects/simple-timer/app.js
@@ -1,51 +1,22 @@
-// create our Timer class
-class Timer {
-	constructor(durationInput, startButton, pauseButton) {
-		// Here we store a reference to our arguments. This way we can easily work with them in the future.
-		this.durationInput = durationInput;
-		this.startButton = startButton;
-		this.pauseButton = pauseButton;
-
-		// Here we bind some event listeners to those elements. This is how we will call the methods that are defined inside of our Timer class.
-		this.startButton.addEventListener("click", this.start);
-		this.pauseButton.addEventListener("click", this.pause);
-	}
-
-	// Here we make the start function an arrow functions. By doing this, the start function gets inserted into the constructor function which gives us access to the Timer object and all its properties.
-	start = () => {
-		this.tick(); // This makes the timer start right away without having to wait a full second before it starts the tick function.
-		this.interval = setInterval(this.tick, 1000); // This run's this.tick once every 1 second.
-	};
-
-	// * In order to share information between different methods, we assign that information to an instance variable. To do this, in the start function we assign "this.interval" equal to the "setInterval" method.
-
-	pause = () => {
-		clearInterval(this.interval);
-	};
-
-	tick = () => {
-		// We access the text input and get the current value, subtract 1 from it and then pass that updated value back into the input.
-		this.timeRemaining = this.timeRemaining - 1; // Here we pass in our "setter" as the variable.
-	};
-
-	// * Behind the scenes, we are calling the "getter" (timeRemaining - 1) to retrieve that value. And we are calling the "setter" (this.timeRemaining) whenever we want to update the value.
-
-	// getter function
-	get timeRemaining() {
-		return parseFloat(this.durationInput.value); // "this.durationInput.value" returns a string value. we need to turn this string value into a number by wrapping it in "parseFloat()".
-	}
-
-	// setter function
-	set timeRemaining(time) {
-		this.durationInput.value = time; // Here we're updating the value of timeRemaining and sending the new value back to the input element.
-	}
-	// * timeRemaining is technically a function but because we put the  "set" keyword in front of the function, whatever the value is that we assign it, is going to be automatically provided as the argument in the "set timeRemaining" function, which in our example is "time".
-}
-
 // Select our 3 elements from the index.html file
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
 // We create our instance of the Timer and pass in the 3 elements we just created
-const timer = new Timer(durationInput, startButton, pauseButton);
+// We need to add an optional 4th argument to our Timer instance, meaning we will still be able to use our timer with or without the 4th argument.
+// Inside the argument object, we're going to pass in a series of different callbacks that we're going to make sure get invoked at very specific times during our timers life cycle.
+const timer = new Timer(durationInput, startButton, pauseButton, {
+	// We need to make sure that these 3 callbacks are all optional.
+	// This is how we are going to hook up our timer to the outside world and allow us to react to the timer starting, a second ticking down, or the timer completing.
+	// This is going to also allow us to segment (isolate) the different parts of our code base.
+	onStart() {
+		console.log("Timer started!");
+	},
+	onTick() {
+		console.log("Timer just ticked down!");
+	},
+	onComplete() {
+		console.log("Timer is completed!");
+	}
+});
---
+++ src/js/projects/simple-timer/timer.js
@@ -0,0 +1,72 @@
+class Timer {
+	constructor(durationInput, startButton, pauseButton, callbacks) {
+		// Here we store a reference to our arguments. This way we can easily work with them in the future.
+		this.durationInput = durationInput;
+		this.startButton = startButton;
+		this.pauseButton = pauseButton;
+
+		// We add a 4th argument to the constructor from our instanced timer's 4th argument with all the callbacks.
+		// We also add a check to make this "callbacks" argument optional.
+		if (callbacks) {
+			// Here we're saving a reference to the callbacks that were passed in.
+			this.onStart = callbacks.onStart;
+			this.onTick = callbacks.onTick;
+			this.onComplete = callbacks.onComplete;
+		}
+
+		// Here we bind some event listeners to those elements. This is how we will call the methods that are defined inside of our Timer class.
+		this.startButton.addEventListener("click", this.start);
+		this.pauseButton.addEventListener("click", this.pause);
+	}
+
+	// Here we make the start function an arrow functions. By doing this, the start function gets inserted into the constructor function which gives us access to the Timer object and all its properties.
+	start = () => {
+		// We need to call the callback and signal that the timer is starting now.
+		// We check if "this.onStart" is defined because it's possible that it won't be defined if we don't pass in any callbacks.
+		if (this.onStart) {
+			// if we do have a callback, then we're going to invoke it inside here.
+			this.onStart();
+		}
+
+		this.tick(); // This makes the timer start right away without having to wait a full second before it starts the tick function.
+		this.interval = setInterval(this.tick, 1000); // This run's this.tick once every 1 second.
+	};
+
+	// * In order to share information between different methods, we assign that information to an instance variable. To do this, in the start function we assign "this.interval" equal to the "setInterval" method.
+
+	pause = () => {
+		clearInterval(this.interval);
+	};
+
+	// This function is going to reach into the text input, get the current value out of it, subtract 1 from it and then pass that updated value back into the input.
+	// The "this.timeRemaining - 1" is pulling the timeRemaining logic from our "timeRemaining()" getter function ("parseFloat(this.durationInput.value)").
+	tick = () => {
+		// We need to add a checker for when the timer hits 0, pause the timer.
+		if (this.timeRemaining <= 0) {
+			this.pause();
+			// Here we invoke our onComplete callback
+			if (this.onComplete) {
+				this.onComplete();
+			}
+		} else {
+			this.timeRemaining = this.timeRemaining - 1; // this.timeRemaining => (setter) = this.timeRemaining - 1 => (getter)
+			// Here we invoke the onTick callback
+			if (this.onTick) {
+				this.onTick();
+			}
+		}
+	};
+
+	// getter function
+	get timeRemaining() {
+		return parseFloat(this.durationInput.value); // "this.durationInput.value" returns a string so we must use "parseFloat" built in JS function to turn the value into a number.
+	}
+
+	// setter function
+	// This setter function changes the value inside the input.
+	set timeRemaining(time) {
+		this.durationInput.value = time;
+	}
+
+	// * we have hidden away all the complexity inside the getter and setter functions.
+}
